02b43ad6b035a0fdcb68d8b87fa9aa8a170407a8
Bogdan Kostrytskyi <ghostautomatic@gmail.com>
2018-11-12 12:55:48 +0200

    added keycode for controller stick

--- src/utils/innerSliderUtils.js
+++ src/utils/innerSliderUtils.js
@@ -298,8 +298,8 @@ export const changeSlide = (spec, options) => {
 export const keyHandler = (e, accessibility, rtl) => {
   if (e.target.tagName.match("TEXTAREA|INPUT|SELECT") || !accessibility)
     return "";
-  if (e.keyCode === 37) return rtl ? "next" : "previous";
-  if (e.keyCode === 39) return rtl ? "previous" : "next";
+  if (e.keyCode === 37 || e.keyCode === 214) return rtl ? "next" : "previous";
+  if (e.keyCode === 39 || e.keyCode === 213) return rtl ? "previous" : "next";
   return "";
 };
 
